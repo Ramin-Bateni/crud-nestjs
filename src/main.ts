63a0e005c6a23dcd44b9c3b92d01c64ffe379060
Mansour Farabi <mansour.farabi@gmail.com>
2025-04-13 22:06:53 +0330

    Refactor main application bootstrap to include enhanced error handling and server startup logging

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.enableCors();
   app.useGlobalPipes(
     new ValidationPipe({
@@ -12,6 +14,22 @@ async function bootstrap() {
       forbidUnknownValues: true,
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Customer CRUD')
+    .setDescription('Simple customer management API')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+
+bootstrap()
+  .then(() => {
+    console.log(`Server is running on port ${process.env.PORT ?? 3000}`);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
