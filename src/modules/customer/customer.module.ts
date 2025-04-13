deae27bb56e01131252b54bf0fb08b2242908dfd
Mansour Farabi <mansour.farabi@gmail.com>
2025-04-14 03:17:23 +0330

    Add customer management endpoints

--- src/modules/customer/customer.module.ts
+++ src/modules/customer/customer.module.ts
@@ -3,6 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { CustomerOrmEntity } from './infrastructure/typeorm/customer.orm-entity';
 import { CustomerRepository } from './infrastructure/typeorm/customer.repository';
 import { CreateCustomerHandler } from './application/commands/handlers/create-customer.handler';
+import { UpdateCustomerHandler } from './application/commands/handlers/update.customer.handler';
+import { DeleteCustomerHandler } from './application/commands/handlers/delete-customer.handler';
+import { GetCustomerHandler } from './application/queries/handlers/get-customer.handler';
+import { GetCustomersHandler } from './application/queries/handlers/get-customers.handler';
 import { CqrsModule } from '@nestjs/cqrs';
 import { CustomerController } from './presentation/controllers/customer.controller';
 
@@ -15,6 +19,10 @@ import { CustomerController } from './presentation/controllers/customer.controll
       useClass: CustomerRepository,
     },
     CreateCustomerHandler,
+    UpdateCustomerHandler,
+    DeleteCustomerHandler,
+    GetCustomerHandler,
+    GetCustomersHandler,
   ],
 })
 export class CustomerModule {}
--- src/modules/customer/presentation/controllers/customer.controller.ts
+++ src/modules/customer/presentation/controllers/customer.controller.ts
@@ -1,14 +1,30 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { CommandBus } from '@nestjs/cqrs';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { CommandBus, QueryBus } from '@nestjs/cqrs';
 import { CreateCustomerDto } from '../../application/dto/create-customer.dto';
 import { CreateCustomerCommand } from '../../application/commands/create-customer.command';
 import { ApiTags, ApiResponse, ApiBadRequestResponse } from '@nestjs/swagger';
 import { Customer } from '../../domain/entities/customer.entity';
+import { GetCustomersQuery } from '../../application/queries/get-customers.query';
+import { GetCustomerQuery } from '../../application/queries/get-customer.query';
+import { DeleteCustomerCommand } from '../../application/commands/delete-customer.command';
+import { UpdateCustomerCommand } from '../../application/commands/update-customer.command';
+import { UpdateCustomerDto } from '../../application/dto/update-customer.dto';
 
 @ApiTags('customers')
 @Controller('customers')
 export class CustomerController {
-  constructor(private readonly commandBus: CommandBus) {}
+  constructor(
+    private readonly commandBus: CommandBus,
+    private readonly queryBus: QueryBus,
+  ) {}
 
   @Post()
   @ApiResponse({ status: 201, description: 'Customer created successfully' })
@@ -18,5 +34,32 @@ export class CustomerController {
   async createCustomer(@Body() dto: CreateCustomerDto): Promise<Customer> {
     const command = new CreateCustomerCommand(dto);
     return await this.commandBus.execute(command);
+  }
+
+  @Get()
+  @ApiResponse({ status: 200, description: 'Customers retrieved successfully' })
+  async getCustomers(): Promise<Customer[]> {
+    return await this.queryBus.execute(new GetCustomersQuery());
+  }
+
+  @Get(':id')
+  @ApiResponse({ status: 200, description: 'Customer retrieved successfully' })
+  async getCustomer(@Param('id') id: string): Promise<Customer> {
+    return await this.queryBus.execute(new GetCustomerQuery(id));
+  }
+
+  @Delete(':id')
+  @ApiResponse({ status: 200, description: 'Customer deleted successfully' })
+  async deleteCustomer(@Param('id') id: string): Promise<void> {
+    return await this.commandBus.execute(new DeleteCustomerCommand(id));
+  }
+
+  @Put(':id')
+  @ApiResponse({ status: 200, description: 'Customer updated successfully' })
+  async updateCustomer(
+    @Param('id') id: string,
+    @Body() dto: UpdateCustomerDto,
+  ): Promise<Customer> {
+    return await this.commandBus.execute(new UpdateCustomerCommand(dto));
   }
 }
