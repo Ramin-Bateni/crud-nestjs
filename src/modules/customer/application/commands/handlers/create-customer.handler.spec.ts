658cb672c4e36677404dddd93eb1e7b0a838c970
Mansour Farabi <mansour.farabi@gmail.com>
2025-04-14 01:39:43 +0330

    Update CreateCustomerHandler tests to use CustomerOrmEntity and modify mock repository methods for consistency

--- src/modules/customer/application/commands/handlers/create-customer.handler.spec.ts
+++ src/modules/customer/application/commands/handlers/create-customer.handler.spec.ts
@@ -4,18 +4,20 @@ import { CreateCustomerDto } from '../../dto/create-customer.dto';
 import { CreateCustomerCommand } from '../create-customer.command';
 // import { Customer } from 'src/modules/customer/domain/entities/customer.entity';
 import { Customer } from '../../../domain/entities/customer.entity';
+import { CustomerOrmEntity } from '../../../infrastructure/typeorm/customer.orm-entity';
 
 const mockRepo = {
-  create: jest.fn<Promise<Customer>, [Customer]>(() =>
-    Promise.resolve({} as Customer),
+  create: jest.fn<Promise<CustomerOrmEntity>, [Customer]>(() =>
+    Promise.resolve({} as CustomerOrmEntity),
   ),
-  findByEmail: jest.fn<Promise<Customer | null>, [string]>(() =>
+  findByEmail: jest.fn<Promise<CustomerOrmEntity | null>, [string]>(() =>
     Promise.resolve(null),
   ),
-  findByIdentity: jest.fn<Promise<Customer | null>, [string, string, Date]>(
-    () => Promise.resolve(null),
-  ),
-  findAll: jest.fn<Promise<Customer[]>, []>(() => Promise.resolve([])),
+  findByIdentity: jest.fn<
+    Promise<CustomerOrmEntity | null>,
+    [string, string, Date]
+  >(() => Promise.resolve(null)),
+  findAll: jest.fn<Promise<CustomerOrmEntity[]>, []>(() => Promise.resolve([])),
 } as jest.Mocked<ICustomerRepository>;
 
 describe('CreateCustomerHandler', () => {
@@ -31,7 +33,7 @@ describe('CreateCustomerHandler', () => {
       firstName: 'Jane',
       lastName: 'Doe',
       dateOfBirth: '1990-01-01',
-      phoneNumber: '+12345678901',
+      phoneNumber: '+989123456789',
       email: 'jane@example.com',
       bankAccountNumber: '1234567890',
     };
@@ -39,7 +41,7 @@ describe('CreateCustomerHandler', () => {
     mockRepo.findByEmail.mockResolvedValue(null);
     mockRepo.findByIdentity.mockResolvedValue(null);
     mockRepo.create.mockImplementation(async (customer) =>
-      Promise.resolve(customer),
+      Promise.resolve(customer as unknown as CustomerOrmEntity),
     );
 
     const result = await handler.execute(new CreateCustomerCommand(dto));
