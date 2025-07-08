2e20d37a5a0e8035e0ca6b225390a1858b352b11
Ramin-Bateni <ram_20designer@yahoo.com>
2025-07-09 00:12:05 +0330

    refactor(domain): integrate Email, PhoneNumber, and BankAccountNumber Value-Objects into Customer entity

--- src/modules/customer/domain/customer.entity.ts
+++ src/modules/customer/domain/customer.entity.ts
@@ -1,12 +1,16 @@
 // src/modules/customer/domain/customer.entity.ts
 
+import { Email } from "./value-objects/email.vo";
+import { PhoneNumber } from "./value-objects/phone-number.vo";
+import { BankAccountNumber } from "./value-objects/bank-account-number.vo";
+
 export class Customer {
   constructor(
     public readonly firstName: string,
     public readonly lastName: string,
     public readonly dateOfBirth: Date,
-    public readonly phoneNumber: string,
-    public readonly email: string,
-    public readonly bankAccountNumber: string
+    public readonly phoneNumber: PhoneNumber,
+    public readonly email: Email,
+    public readonly bankAccountNumber: BankAccountNumber
   ) {}
 }
