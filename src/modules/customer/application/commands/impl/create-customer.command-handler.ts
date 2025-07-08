0b332c1d016b86a8a285d508cac03f15e2de4d71
Ramin-Bateni <ram_20designer@yahoo.com>
2025-07-08 20:03:35 +0330

    feat(app): implement execute method to create new Customer using repository and passing unit tests (TDD green)

--- src/modules/customer/application/commands/impl/create-customer.command-handler.ts
+++ src/modules/customer/application/commands/impl/create-customer.command-handler.ts
@@ -3,6 +3,7 @@
 import { CommandHandler, ICommandHandler } from "@nestjs/cqrs";
 import { CreateCustomerCommand } from "./create-customer.command";
 import { ICustomerRepository } from "@/modules/customer/application/interfaces/customer-repository.interface";
+import { Customer } from "@/modules/customer/domain/customer.entity";
 
 @CommandHandler(CreateCustomerCommand)
 export class CreateCustomerCommandHandler
@@ -11,6 +12,15 @@ export class CreateCustomerCommandHandler
   constructor(private readonly repository: ICustomerRepository) {}
 
   async execute(command: CreateCustomerCommand): Promise<void> {
-    // TODO: Not implemented yet
+    const customer = new Customer(
+      command.firstName,
+      command.lastName,
+      command.dateOfBirth,
+      command.phoneNumber,
+      command.email,
+      command.bankAccountNumber
+    );
+
+    await this.repository.create(customer);
   }
 }
