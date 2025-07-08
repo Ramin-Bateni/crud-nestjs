b3cc1e166067a7721d80891922b4cf69aa53d32c
Ramin-Bateni <ram_20designer@yahoo.com>
2025-07-09 02:25:21 +0330

    feat(bdd): implement step definitions for creating a customer (Given/When/Then - partial, red)

--- tests/features/customer/steps/create-customer.step.ts
+++ tests/features/customer/steps/create-customer.step.ts
@@ -1,18 +1,17 @@
 import { Given, When, Then } from "@cucumber/cucumber";
 import { strict as assert } from "assert";
+import request from "supertest";
+import { Test } from "@nestjs/testing";
+import { AppModule } from "@/app.module";
+import { INestApplication } from "@nestjs/common";
 
-type Body = {
-  id: number;
-  email: string;
-};
-let response: {
-  status: Number;
-  body: Body;
-};
+let app: INestApplication;
+let response: request.Response;
 let customerPayload: any;
 
-Given("a valid customer with the following data:", (dataTable) => {
+Given("a valid customer with the following data:", async (dataTable) => {
   const data = dataTable.hashes()[0];
+
   customerPayload = {
     firstName: data.FirstName,
     lastName: data.LastName,
@@ -21,20 +20,27 @@ Given("a valid customer with the following data:", (dataTable) => {
     email: data.Email,
     bankAccountNumber: data.BankAccountNumber,
   };
+
+  const moduleRef = await Test.createTestingModule({
+    imports: [AppModule],
+  }).compile();
+
+  app = moduleRef.createNestApplication();
+
+  await app.init();
 });
 
 When("the client sends a request to create the customer", async () => {
-  response = {
-    status: 201,
-    body: { id: "mock-id", ...customerPayload },
-  };
+  response = await request(app.getHttpServer())
+    .post("/customers")
+    .send(customerPayload);
 });
 
 Then("the customer should be saved in the system", () => {
-  assert.ok(response.body.id);
   assert.equal(response.body.email, customerPayload.email);
+  expect(response.statusCode).toBe(201);
 });
 
-Then("a 201 Created response should be returned", () => {
-  assert.equal(response.status, 201);
+Then("a 201 Created response should be returned", (statusCode) => {
+  expect(response.statusCode).toBe(statusCode);
 });
