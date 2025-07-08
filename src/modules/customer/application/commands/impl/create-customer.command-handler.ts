a1383f1c9c6100b9549d10106d48f9de4918bdd7
Ramin-Bateni <ram_20designer@yahoo.com>
2025-07-09 00:33:06 +0330

    refactor(app): change handler to build Customer using value objects

--- src/modules/customer/application/commands/impl/create-customer.command-handler.ts
+++ src/modules/customer/application/commands/impl/create-customer.command-handler.ts
@@ -4,6 +4,9 @@ import { CommandHandler, ICommandHandler } from "@nestjs/cqrs";
 import { CreateCustomerCommand } from "./create-customer.command";
 import { ICustomerRepository } from "@/modules/customer/application/interfaces/customer-repository.interface";
 import { Customer } from "@/modules/customer/domain/customer.entity";
+import { Email } from "@/modules/customer/domain/value-objects/email.vo";
+import { PhoneNumber } from "@/modules/customer/domain/value-objects/phone-number.vo";
+import { BankAccountNumber } from "@/modules/customer/domain/value-objects/bank-account-number.vo";
 
 @CommandHandler(CreateCustomerCommand)
 export class CreateCustomerCommandHandler
@@ -16,9 +19,9 @@ export class CreateCustomerCommandHandler
       command.firstName,
       command.lastName,
       command.dateOfBirth,
-      command.phoneNumber,
-      command.email,
-      command.bankAccountNumber
+      new PhoneNumber(command.phoneNumber),
+      new Email(command.email),
+      new BankAccountNumber(command.bankAccountNumber)
     );
 
     await this.repository.create(customer);
