64bc6b6aaef0454cb4f5371cd28a937944e4de71
Ramin-Bateni <ram_20designer@yahoo.com>
2025-07-09 04:04:27 +0330

    refactor(interface): create a token name (I_CUSTOMER_REPOSITORY) for ICustomerRepository to use in dependency injection

--- src/modules/customer/application/interfaces/customer-repository.interface.ts
+++ src/modules/customer/application/interfaces/customer-repository.interface.ts
@@ -2,6 +2,8 @@
 
 import { Customer } from "@/modules/customer/domain/customer.entity";
 
+export const I_CUSTOMER_REPOSITORY = Symbol("ICustomerRepository");
+
 export interface ICustomerRepository {
   create(customer: Customer): Promise<void>;
 }
