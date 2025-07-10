23226a7a4e3b7bd5d6550cc6163316d219ccbe0a
Ramin-Bateni <ram_20designer@yahoo.com>
2025-07-10 21:21:23 +0330

    feat(customer): register CustomerSchema in CustomerModule + Add CustomerRepository to providers

--- src/modules/customer/customer.module.ts
+++ src/modules/customer/customer.module.ts
@@ -1,25 +1,34 @@
 // src/modules/customer/customer.module.ts
 import { Module } from "@nestjs/common";
 import { CqrsModule } from "@nestjs/cqrs";
-import { CustomerController } from "./presentation/customer.controller";
+// Infrastructure layer
+import { CustomerRepository } from "./infrastructure/repositories/customer.repository";
+import { MongooseModule } from "@nestjs/mongoose";
+import {
+  Customer,
+  CustomerSchema,
+} from "./infrastructure/repositories/schemas/customer.schema";
+// Application layer
 import { CreateCustomerCommandHandler } from "./application/commands/impl/create-customer.command-handler";
 import { CustomerService } from "./application/services/customer.service";
-import { I_CUSTOMER_REPOSITORY } from "./application/interfaces/customer-repository.interface";
 import { GetAllCustomersQueryHandler } from "./application/queries/impl/get-all-customers.query-handler";
+// Presentation layer
+import { CustomerController } from "./presentation/customer.controller";
 
 @Module({
+  imports: [
+    CqrsModule,
+    MongooseModule.forFeature([
+      { name: Customer.name, schema: CustomerSchema },
+    ]),
+  ],
   providers: [
     CreateCustomerCommandHandler,
     GetAllCustomersQueryHandler,
+    CustomerRepository,
     CustomerService,
-    {
-      provide: I_CUSTOMER_REPOSITORY,
-      useValue: {
-        create: async () => {}, // mock function to temporally have Green BDD before a real DB
-      },
-    },
   ],
-  imports: [CqrsModule],
+  exports: [CustomerService, CustomerRepository],
   controllers: [CustomerController],
 })
 export class CustomerModule {}
