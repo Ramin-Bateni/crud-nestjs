b8eda3fe6b78a2e8d29f3ace8baa819d9d5adcdc
Ramin-Bateni <ram_20designer@yahoo.com>
2025-07-10 21:15:49 +0330

    feat(database): load MongoDB URI via forRootAsync in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 // src/app.module.ts
 import { Module } from "@nestjs/common";
 import { CustomerModule } from "./modules/customer/customer.module";
+import { MongooseModule } from "@nestjs/mongoose";
 
 @Module({
-  imports: [CustomerModule],
+  imports: [
+    // Connect to MongoDB database
+    MongooseModule.forRoot(
+      process.env.MONGO_URI || "mongodb://localhost:27017/crud-nestjs"
+    ),
+    CustomerModule,
+  ],
   controllers: [],
   providers: [],
 })
