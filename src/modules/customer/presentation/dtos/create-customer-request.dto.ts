9dbf82a1830f8a5b1970d7f82af84100a9380c18
Ramin-Bateni <ram_20designer@yahoo.com>
2025-07-10 21:23:48 +0330

    feat(presentation): add CreateCustomerRequestDto

---
+++ src/modules/customer/presentation/dtos/create-customer-request.dto.ts
@@ -0,0 +1,20 @@
+import { Customer } from "../../domain/customer.entity";
+
+export class CreateCustomerRequestDto {
+  firstName: string;
+  lastName: string;
+  dateOfBirth: string;
+  phoneNumber: string;
+  email: string;
+  bankAccountNumber: string;
+
+  constructor(domainCustomer: Customer) {
+    this.firstName = domainCustomer.firstName;
+    this.lastName = domainCustomer.lastName;
+    // Format date as YYYY-MM-DD
+    this.dateOfBirth = domainCustomer.dateOfBirth.toISOString().split("T")[0];
+    this.phoneNumber = domainCustomer.phoneNumber.toString();
+    this.email = domainCustomer.email.getValue();
+    this.bankAccountNumber = domainCustomer.bankAccountNumber.getValue();
+  }
+}
